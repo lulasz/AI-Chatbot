d663cee833fc1144b54c20928a82cb3c803b3e99
Lukasz Majewski <lulasz47@outlook.com>
2024-10-26 22:56:29 +0200

    Recorder - proper states

--- audio_recorder.py
+++ audio_recorder.py
@@ -11,17 +11,17 @@ class AudioRecorder:
         self.chunk_duration = 0.3  # Duration of each audio chunk in seconds
         self.pre_buffer_duration = 0.5  # Duration to pre-buffer audio in seconds
         self.recording = False  # Track if currently recording
-        self.state = "Starting..."
+        self.state = "start"
         
     def record(self):
         self.recording = True  # Set recording state
         # Pre-buffering audio
-        self.state = "Preparing to record..."
+        self.state = "prepare"
         pre_buffer = sd.rec(int(self.pre_buffer_duration * self.samplerate), samplerate=self.samplerate, channels=1, dtype='float64')
         sd.wait()  # Wait for the pre-buffering to finish
         self.audio_data.append(pre_buffer)  # Append pre-buffered audio
 
-        self.state = "Recording started..."
+        self.state = "record"
         while self.recording:
             # Record a chunk of audio
             chunk = sd.rec(int(self.chunk_duration * self.samplerate), samplerate=self.samplerate, channels=1, dtype='float64')
@@ -34,7 +34,7 @@ class AudioRecorder:
             if average_amplitude < self.threshold:
                 self.silent_time += self.chunk_duration  # Increment silent time
                 if self.silent_time >= self.silence_duration:  # Check if silence duration exceeds limit
-                    self.state = "Silence detected. Stopping recording."
+                    self.state = "silence"
                     self.recording = False  # Stop recording
             else:
                 self.silent_time = 0  # Reset silence counter if sound is detected
@@ -42,7 +42,7 @@ class AudioRecorder:
         # Concatenate recorded audio chunks and convert to int16
         self.audio_data = np.concatenate(self.audio_data)
         self.audio_data = np.clip(self.audio_data * 32767, -32768, 32767).astype(np.int16)  # Clamping values to prevent overflow
-        self.state = "Recording completed and stored in memory."
+        self.state = "done"
 
     def get_audio_data(self):
         return self.audio_data
